aa14af57e78ba57082a3bfc1ba609df48e6bebf8
eonezhang <eonezhang@qq.com>
2018-01-08 22:31:23 +0800

    test

--- src/com/eonezhang/jenkins/pipelineshared/Utils.groovy
+++ src/com/eonezhang/jenkins/pipelineshared/Utils.groovy
@@ -5,4 +5,10 @@ def helloWorld() {
 	return "say hello from shared lib"
 }
 
+def getProjectVersion() {
+	def file = readFile('pom.xml')
+	def project = new XmlSlurper().parseText(file)
+	return project.version.text()
+}
+
 return this;
