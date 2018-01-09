db2c2a1d07c79309e0fbd6294e9432be9ce0da5d
eonezhang <eonezhang@qq.com>
2018-01-09 10:16:15 +0800

    test

--- src/com/eonezhang/jenkins/pipelineshared/Utils.groovy
+++ src/com/eonezhang/jenkins/pipelineshared/Utils.groovy
@@ -9,6 +9,7 @@ def helloWorld() {
 @NonCPS
 def getProjectVersion() {
 	def workspace = manager.build.getEnvVars()["WORKSPACE"]
+	println workspace
 	def file = readFile("${workspace}/pom.xml")
 	def project = new XmlSlurper().parseText(file)
 	return project.version.text()
